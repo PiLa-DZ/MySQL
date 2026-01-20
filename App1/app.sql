1f9701171eeefb1eefef2ca910491cd4c066b9f9
Nabil <Nabil@gmail.com>
2026-01-20 10:47:18 +0100

    First Commit

---
+++ App1/app.sql
@@ -0,0 +1,13 @@
+-- = Create Database =============================
+-- create database Facebook;
+
+-- = Use Tatabase ================================
+-- use Facebook;
+
+-- = Show All Tables =============================
+show tables;
+create table users (
+    id INT auto_increment primary key,
+    name varchar(100),
+    age INT
+);
